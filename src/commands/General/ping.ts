e830cf9302eecd5c6b1b9c943d8bf84d43ad8348
acollierr17 <gamemaniac101@gmail.com>
2020-09-20 19:18:07 -0400

    :sparkles: Add in first commands and events

---
+++ src/commands/General/ping.ts
@@ -0,0 +1,33 @@
+import { Message, GuildChannel } from 'eris';
+import { oneLine } from 'common-tags';
+
+import Command from '../../structures/command';
+import SuggestionsClient from '../../structures/client';
+
+export default class PingCommand extends Command {
+  constructor(public client: SuggestionsClient) {
+    super(client);
+
+    this.name = 'ping';
+    this.category = 'General';
+    this.description = 'View the latency of the bot and it\'s connection to the Discord API.';
+    this.aliases = ['pong'];
+    this.guildOnly = false;
+    this.guarded = true;
+  }
+
+  public async run(message: Message, args: Array<string>): Promise<any> {
+    try {
+      const ping = message.channel instanceof GuildChannel ? message.channel.guild.shard.latency : this.client.shards.get(0).latency;
+      const msg = await message.channel.createMessage('🏓 Ping!');
+      return msg.edit(oneLine`
+        Pong! 
+        Latency is \`${msg.timestamp - message.timestamp}ms\`.
+        API Latency is \`${Math.round(ping)}ms\`.
+      `);
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
---
+++ src/listeners/ready.ts
@@ -0,0 +1,27 @@
+import Event from '../structures/event';
+
+import SuggestionsClient from '../structures/client';
+import { version } from '../../package.json';
+
+export default class extends Event {
+  constructor(public client: SuggestionsClient, public name: string) {
+    super(client, name, {
+      once: true
+    });
+
+    this.name = 'ready';
+  }
+
+  public async run(): Promise<any> {
+    const readyMessages: Array<string> = [
+      `🏷 Version ${version} of the bot loaded in ${process.env.NODE_ENV.toUpperCase().trim()}.`,
+      `🤖 Logged in as ${this.client.user.username}#${this.client.user.discriminator} (${this.client.user.id}).`,
+      `⚙ Loaded (${this.client.commands.size}) commands!`,
+      `⚙ Loaded (${this.client.subCommands.size}) subcommands!`,
+      `👂 Loaded (${this.client.events.size}) events!`
+    ];
+
+    for (const m of readyMessages) await console.log(m);
+    this.client.updateBotPresence();
+  }
+}

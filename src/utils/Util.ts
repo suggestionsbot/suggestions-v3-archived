004d2626c962105bcd2f2a5e266484536b446568
acollierr17 <gamemaniac101@gmail.com>
2020-09-22 02:04:24 -0400

    :sparkles: Add in permissions utils for parsing

--- src/utils/Util.ts
+++ src/utils/Util.ts
@@ -1,7 +1,7 @@
+import Permissions from './Permissions';
+
 export default class Util {
   public static formatPermission(permission: string): string {
-    const result = permission.split(/(?=[A-Z])/).join(' ');
-
-    return result.charAt(0).toUpperCase() + result.slice(1);
+    return Permissions[permission];
   }
 }

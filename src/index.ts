1c2e293eed9d525ef296fa994610b100dab81073
acollierr17 <gamemaniac101@gmail.com>
2020-09-26 01:58:35 -0400

    :construction: WIP Implement sharding

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,39 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import SuggestionsClient from './structures/client';
 import Logger from './utils/Logger';
 
 export const main = async (): Promise<boolean> => {
   try {
-    const client = new SuggestionsClient(process.env.DISCORD_TOKEN);
-    client.start();
-    // const sharder = new Master(process.env.DISCORD_TOKEN, '/dist/lib/src/structures/client.js', {
-    //   stats: false,
-    //   debug: true,
-    //   clusters: 1,
-    //   name: 'Suggestions',
-    //   clientOptions: {
-    //     messageLimit: 150,
-    //     defaultImageFormat: 'png'
-    //   }
-    // });
-    //
-    // sharder.on('stats', stats => {
-    //   Logger.debug('SHARDS', stats);
-    // });
+    const mainFile = process.env.NODE_ENV === 'production' ?
+      '/dist/lib/src/structures/shard.js' :
+      '/src/structures/shard.ts';
+
+    const sharder = new Master(process.env.DISCORD_TOKEN, mainFile, {
+      stats: false,
+      debug: true,
+      clusters: 1,
+      // TODO look into properly setting shards to 'auto': https://canary.discordapp.com/channels/364124474729037824/364132890788757514/759218710522232832
+      shards: 1,
+      guildsPerShard: 1500,
+      name: 'Suggestions',
+      webhooks: {
+        shard: {
+          id: '759222460234727465',
+          token: 'jBafo5oVcoKyWYPlCM7lryPv1BYyYJClpJC6XT5zaa7bjFeRircACHTmrM4iaPcwLbF_',
+        },
+        cluster: {
+          id: '759222703831515148',
+          token: 'XRFEomcp7clM99BH-BQ4bLLFoPva0ezive-f1M88OO-N3_YtAzX4mOYYmKCKALAUoNI_'
+        }
+      },
+      clientOptions: {
+        messageLimit: 150,
+        defaultImageFormat: 'png'
+      }
+    });
+
+    sharder.on('stats', stats => Logger.log(stats));
 
     return true;
   } catch (error) {

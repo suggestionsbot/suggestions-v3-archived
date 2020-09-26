a2977ee18aaf9b47f35a0f7fbdf25bfdd0f9c8fe
acollierr17 <gamemaniac101@gmail.com>
2020-09-26 01:56:59 -0400

    :recycle: Minor code improvements

--- src/provider/redis/index.ts
+++ src/provider/redis/index.ts
@@ -19,13 +19,13 @@ export default class Redis {
     this.redis = asyncRedis.decorate(createClient({
       host: process.env.REDIS_HOSTNAME,
       password: process.env.REDIS_PASSWORD,
-      port: parseInt(process.env.REDIS_PORT)
+      port: +process.env.REDIS_PORT
     }));
 
     this.helpers = new RedisHelpers(this.client, this.redis);
 
     this.redis.on('ready', () => {
-      Logger.ready('REDIS Connection successfully opened!');
+      Logger.ready('Redis connection successfully opened!');
     });
   }
 }

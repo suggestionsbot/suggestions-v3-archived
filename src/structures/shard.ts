359aa078f6337461be8cf529c635a9bbdd0777d4
acollierr17 <gamemaniac101@gmail.com>
2020-09-27 23:47:10 -0400

    :sparkles: Add in Sentry DSN for error reporting

--- src/structures/shard.ts
+++ src/structures/shard.ts
@@ -1,3 +1,25 @@
+import * as Sentry from '@sentry/node';
+import { CaptureConsole, RewriteFrames } from '@sentry/integrations';
+import { version } from '../../package.json';
+import dotenv from 'dotenv';
+dotenv.config();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: parseFloat(process.env.SENTRY_TRACES_SAMPLERATE),
+  release: version,
+  environment: process.env.NODE_ENV,
+  maxBreadcrumbs: 30,
+  integrations: [
+    new RewriteFrames({
+      root: global.__rootdir__
+    }),
+    new CaptureConsole({
+      levels: ['error']
+    })
+  ]
+});
+
 import { Base } from 'eris-sharder';
 
 import SuggestionsClient from './client';
@@ -14,6 +36,7 @@ export default class ShardClient extends Base {
 
   public launch(): any {
     this._client.base = this;
+    this._client.sentry = Sentry;
 
     this.ipc.register('changeStatus', status => {
       this._client.editStatus(status.status, {

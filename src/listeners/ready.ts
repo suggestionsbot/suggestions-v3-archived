452ae4a415036d957f9fc6fa28a3e34a7ad8356c
acollierr17 <gamemaniac101@gmail.com>
2020-10-07 22:30:44 -0400

    :recycle: Bit of refactoring and overall tidiness

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -3,6 +3,7 @@ import Event from '../structures/Event';
 import SuggestionsClient from '../structures/Client';
 import { version } from '../../package.json';
 import Logger from '../utils/Logger';
+import Util from '../utils/Util';
 
 export default class extends Event {
   constructor(public client: SuggestionsClient, public name: string) {
@@ -16,7 +17,6 @@ export default class extends Event {
   public async run(): Promise<any> {
     const readyMessages: Array<string> = [
       `🔖 Version ${version} of the bot loaded in ${process.env.NODE_ENV.toUpperCase().trim()}.`,
-      `🤖 Logged in as ${this.client.user.username}#${this.client.user.discriminator} (${this.client.user.id}).`,
       `⚙ Loaded (${this.client.commands.size}) commands!`,
       `⚙ Loaded (${this.client.subCommands.size}) subcommands!`,
       `👂 Loaded (${this.client.events.size}) events!`,
@@ -26,7 +26,8 @@ export default class extends Event {
     try {
       await this.client.database.init();
       await this.client.redis.init();
-      for (const m of readyMessages) await Logger.ready(m);
+      for (const m of readyMessages) await Logger.event(this.name, m);
+      Logger.ready(`🤖 Logged in as ${Util.formatUserTag(this.client.user)} (${this.client.user.id}).`);
       this.client.updateBotPresence();
     } catch (e) {
       Logger.error('READY EVENT', e);

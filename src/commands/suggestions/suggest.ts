25dac3cac8a5ee1efe230996d8bb8a21c183d422
acollierr17 <gamemaniac101@gmail.com>
2020-10-17 00:08:35 -0400

    :art: Add in required preconditions

--- src/commands/suggestions/suggest.ts
+++ src/commands/suggestions/suggest.ts
@@ -1,12 +1,11 @@
 import Command from '../../structures/Command';
 import SuggestionsClient from '../../structures/Client';
 import Context from '../../structures/Context';
-import { CommandNextFunction, SuggestionType } from '../../types';
+import { CommandNextFunction, SuggestionChannelType, SuggestionType } from '../../types';
 import MessageUtils from '../../utils/MessageUtils';
 import { stripIndents } from 'common-tags';
 import * as crypto from 'crypto';
 import Util from '../../utils/Util';
-import { GuildTextableChannel } from 'eris';
 import Logger from '../../utils/Logger';
 
 export default class SuggestCommand extends Command {
@@ -35,23 +34,65 @@ export default class SuggestCommand extends Command {
 
   async runPreconditions(ctx: Context, next: CommandNextFunction): Promise<any> {
     if (!ctx.args[0]) return MessageUtils.error(ctx.client, ctx.message, 'Please provide text for this suggestion!');
+    const channels = ctx.settings!.channels.map(c => c.channel);
+    const channel = channels.length <= 1 ? channels[0] : ctx.args[0];
+    const gChannel = Util.getChannel(channel, ctx.guild!);
+    if (!gChannel && (channels.length > 1))
+      return MessageUtils.error(this.client, ctx.message, `\`${ctx.args[0]}\` is not a valid channel!`);
+    if (gChannel && (channels.length > 1) && !channels.includes(gChannel.id))
+      return MessageUtils.error(this.client, ctx.message,stripIndents`${gChannel.mention} is not a valid suggestions channel!
+        
+        Valid channels: ${channels.map(c => `<#${c}>`).join(' ')}
+      `);
+
+    const sChannel = this.client.suggestionChannels.get(gChannel!.id);
+    if (!sChannel) return MessageUtils.error(this.client, ctx.message,
+      `Cannot post to ${gChannel!.mention} as it's not currently available!`
+    );
+    if (!this.client.isAdmin(ctx.member!)) {
+      if (sChannel.type === SuggestionChannelType.STAFF && !this.client.isStaff(ctx.member!, ctx.settings!))
+        return MessageUtils.error(this.client, ctx.message,
+          `Cannot post to ${sChannel.channel.mention} as it is a staff suggestion channel!`
+        );
+      if (sChannel.locked) return MessageUtils.error(this.client, ctx.message,
+        `Cannot post to ${sChannel.channel.mention} as it is currently locked!`
+      );
+      if ((sChannel.blocked.size > 0) && sChannel.blocked.some(r => ctx.member!.roles.includes(r.id)))
+        return MessageUtils.error(this.client, ctx.message,
+          stripIndents`You cannot submit suggestions to ${sChannel.channel.mention} as you are in a blocked role!
+          
+          **Blocked:** ${sChannel.blocked.map(r => r.mention).join(' ')}`
+        );
+      if ((sChannel.allowed.size > 0) && (!sChannel.allowed.some(r => ctx.member!.roles.includes(r.id))))
+        return MessageUtils.error(this.client, ctx.message,
+          `You cannot submit suggestions to ${sChannel.channel.mention} as you are not in an allowed role!
+        
+        **Allowed:** ${sChannel.allowed.map(r => r.mention).join(' ')}`
+        );
+    }
+    if (![SuggestionChannelType.SUGGESTIONS, SuggestionChannelType.STAFF].includes(sChannel.type))
+      return MessageUtils.error(this.client, ctx.message, `Suggestions cannot be posted in channels with the type: \`${sChannel.type}\`.`);
+
     next();
   }
 
   async run(ctx: Context): Promise<any> {
-    const suggestion = ctx.args.join(' ');
+    const argCheck = Util.getChannel(ctx.args[0], ctx.guild!);
+    const channels = ctx.settings!.channels.map(c => c.channel);
+    const channel = Util.getChannel(channels.length <= 1 ? channels[0] : ctx.args[0], ctx.guild!)!;
+    const sChannel = this.client.suggestionChannels.get(channel.id)!;
+
+    const suggestion = argCheck ? ctx.args.slice(1).join(' ') : ctx.args.join(' ');
 
     const imageCheck = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/.exec(suggestion);
     const id = crypto.randomBytes(20).toString('hex');
     const shortID = id.slice(33, 40);
 
-    const channel = <GuildTextableChannel>ctx.guild!.channels.get('737167291656962060');
-
     const embed = MessageUtils.defaultEmbed()
       .setDescription(stripIndents`
           **Submitter**
           ${Util.escapeMarkdown(Util.formatUserTag(ctx.sender))}
-          
+
           **Suggestion**
           ${suggestion}
         `)
@@ -63,10 +104,10 @@ export default class SuggestCommand extends Command {
 
     const dmEmbed = MessageUtils.defaultEmbed()
       .setAuthor(ctx.guild!.name, ctx.guild!.iconURL)
-      .setDescription(stripIndents`Hey, ${ctx.sender.mention}. Your suggestion has been sent to the ${channel.mention} to be voted on!
-      
+      .setDescription(stripIndents`Hey, ${ctx.sender.mention}. Your suggestion has been sent to the ${sChannel.channel.mention} to be voted on!
+
       Please wait until it gets approved or rejected by a staff member.
-      
+
       Your suggestion ID (sID) for reference is **${shortID}**.
       `)
       .setFooter(`Guild ID: ${ctx.guild!.id} | sID: ${shortID}`)
@@ -74,24 +115,21 @@ export default class SuggestCommand extends Command {
 
     const emojis = ['✅', '❎'];
 
-    const submitted = await channel.createMessage({ embed });
-
-    for (const emoji of emojis) await submitted.addReaction(emoji);
 
     try {
-      await ctx.dm({
-        user: ctx.sender,
-        embed: dmEmbed
-      });
-    } catch (e) {
-      if (e.message === 'Missing Access') return;
-    }
 
-    try {
-      await this.client.database.suggestionHelpers.createSuggestion({
-        guild: ctx.guild!.id,
+
+      const submitted = await sChannel.channel.createMessage({ embed });
+
+      for (const emoji of emojis) await submitted.addReaction(emoji);
+
+      // await ctx.dm({
+      //   user: ctx.sender,
+      //   embed: dmEmbed
+      // });
+
+      await sChannel.createSuggestion({
         user: ctx.sender.id,
-        channel: channel.id,
         message: submitted.id,
         suggestion,
         id,
@@ -99,6 +137,7 @@ export default class SuggestCommand extends Command {
       });
       await MessageUtils.delete(ctx.message, { timeout: 5000 });
     } catch (e) {
+      if (e.message === 'Missing Access') return;
       Logger.error(e);
       return MessageUtils.error(ctx.client, ctx.message, e.message, true);
     }

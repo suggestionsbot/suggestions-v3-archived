bd9e24069241df57a894bf390c2e6e932fb64318
acollierr17 <gamemaniac101@gmail.com>
2020-10-23 17:08:50 -0400

    :art: Remove redundant type validation

--- src/utils/MessageEmbed.ts
+++ src/utils/MessageEmbed.ts
@@ -29,21 +29,18 @@ export default class MessageEmbed {
   }
 
   public setTitle(title: string): this {
-    if (typeof title !== 'string') throw new TypeError(`Expected type 'string', received type '${typeof title}'`);
     if (title.length > 256) throw new RangeError('Embed titles cannot exceed 356 characters');
     this.title = title;
     return this;
   }
 
   public setDescription(description: string): this {
-    if (typeof description !== 'string') throw new TypeError(`Expected type 'string', received type '${typeof description}'`);
     if (description.length > 2048) throw new RangeError('Embed titles cannot exceed 2048 characters');
     this.description = description;
     return this;
   }
 
   public setURL(url: string): this {
-    if (typeof url !== 'string') throw new TypeError(`Expected type 'string', received type '${typeof url}'`);
     if (!this._URL_REGEX.test(url)) throw new Error('Not a well formed URL');
     this.url = url;
     return this;
@@ -56,7 +53,6 @@ export default class MessageEmbed {
   }
 
   public setColor(color: string|number): this {
-    if (typeof color !== 'string' && typeof color !== 'number') throw new TypeError(`Expected types 'string' or 'number', received type ${typeof color} instead`);
     if (typeof color === 'number') {
       if (color > 16777215 || color < 0) throw new RangeError('Invalid color');
       this.color = color;
@@ -70,12 +66,10 @@ export default class MessageEmbed {
   }
 
   public setFooter(text: string, iconURL?: string): this {
-    if (typeof text !== 'string') throw new TypeError(`Expected type 'string', received type ${typeof text}`);
     if (text.length > 2048) throw new RangeError('Embed footer texts cannot exceed 2048 characters');
     this.footer = { text };
 
     if (iconURL !== undefined) {
-      if (typeof iconURL !== 'string') throw new TypeError(`Expected type 'string', received type '${typeof iconURL}'`);
       if (!iconURL.startsWith('attachment://') && !this._URL_REGEX.test(iconURL)) throw new Error('Not a well formed URL');
       this.footer.icon_url = iconURL;
     }
@@ -84,32 +78,27 @@ export default class MessageEmbed {
   }
 
   public setImage(imageURL: string): this {
-    if (typeof imageURL !== 'string') throw new TypeError(`Expected type 'string', received type ${typeof imageURL}`);
     if (!imageURL.startsWith('attachment://') && !this._URL_REGEX.test(imageURL)) throw new Error('Not a well formed URL');
     this.image = { url: imageURL };
     return this;
   }
 
   public setThumbnail(thumbnailURL: string): this {
-    if (typeof thumbnailURL !== 'string') throw new TypeError(`Expected type 'string', received type ${typeof thumbnailURL}`);
     if (!thumbnailURL.startsWith('attachment://') && !this._URL_REGEX.test(thumbnailURL)) throw new Error('Not a well formed URL');
     this.thumbnail = { url: thumbnailURL };
     return this;
   }
 
   public setAuthor(name: string, iconURL?: string, url?: string): this {
-    if (typeof name !== 'string') throw new TypeError(`Expected type 'string', received type ${typeof name}`);
     if (name.length > 256) throw new RangeError('Embed footer texts cannot exceed 2048 characters');
     this.author = { name };
 
     if (iconURL !== undefined) {
-      if (typeof iconURL !== 'string') throw new TypeError(`Expected type 'string', received type '${typeof iconURL}'`);
       if (!iconURL.startsWith('attachment://') && !this._URL_REGEX.test(iconURL)) throw new Error('Not a well formed URL');
       this.author.icon_url = iconURL;
     }
 
     if (url !== undefined) {
-      if (typeof url !== 'string') throw new TypeError(`Expected type 'string', received type '${typeof url}'`);
       if (!this._URL_REGEX.test(url)) throw new Error('Not a well formed URL');
       this.author.url = url;
     }
@@ -120,9 +109,6 @@ export default class MessageEmbed {
 
   public addField(name: string, value: string, inline = false): this {
     if (this.fields.length >= 25) throw new RangeError('Embeds cannot contain more than 25 fields');
-    if (typeof name !== 'string') throw new TypeError(`Expected type 'string', received type ${typeof name}`);
-    if (typeof value !== 'string') throw new TypeError(`Expected type 'string', received type ${typeof value}`);
-    if (typeof inline !== 'boolean') throw new TypeError(`Expected type 'boolean', received type ${typeof inline}`);
     if (name.length > 256) throw new RangeError('Embed field names cannot exceed 256 characters');
     if (value.length > 1024) throw new RangeError('Embed field names cannot exceed 256 characters');
 

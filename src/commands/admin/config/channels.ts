284bf6c1413c32a1fe35cb4fc1feedd1224d589e
acollierr17 <gamemaniac101@gmail.com>
2021-01-29 19:39:31 -0500

    :recycle: Use context getSettings method

--- src/commands/admin/config/channels.ts
+++ src/commands/admin/config/channels.ts
@@ -322,7 +322,7 @@ export default class ConfigChannelsCommand extends SubCommand {
           try {
             const channel = ctx.args.get(1);
             const gChannel = Util.getChannel(channel, ctx.guild!)!;
-            const data = await this.client.database.helpers.guild.getGuild(ctx.guild!, false);
+            const data = await ctx.getSettings(false)!;
             const updated = <SuggestionChannelSchema>{
               id: gChannel.id,
               type: type ? <SuggestionChannelType>type : SuggestionChannelType.SUGGESTIONS,

42980648962c1df65e50fcfbd40b2e2d9c82108a
acollierr17 <gamemaniac101@gmail.com>
2021-02-01 12:20:26 -0500

    :sparkles: Add variables for truthy/falsely config command options

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,3 +1,5 @@
 export const BULLET_POINT = '•';
 
 export const CONFIG_OPTIONS = ['true', 'on', 'false', 'off', 'toggle'];
+export const TRUTHY_CONFIG_OPTIONS = ['true', 'on'];
+export const FALSEY_CONFIG_OPTIONS = ['false', 'off'];
